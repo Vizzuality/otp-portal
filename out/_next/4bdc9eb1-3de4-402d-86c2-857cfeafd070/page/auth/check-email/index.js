bf753b6030388f5657b039aee85aedd9003ca9a1
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-06-15 11:30:17 +0200

    New build

---
+++ out/_next/4bdc9eb1-3de4-402d-86c2-857cfeafd070/page/auth/check-email/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/auth/check-email', function() {
+            var comp = module.exports=webpackJsonp([13],{392:function(e,t,u){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(5),r=a(n),i=u(2),l=a(i),s=u(3),o=a(s),c=u(7),f=a(c),d=u(6),p=a(d),m=u(0),_=a(m),h=u(1),b=a(h),k=u(27),y=a(k),N=u(28),j=u(34),v=a(j),C=u(33),E=a(C),g="/Users/barrenechea/Projects/otp-portal/pages/auth/check-email.js?entry",w=function(e){function t(){return(0,l.default)(this,t),(0,f.default)(this,(t.__proto__||(0,r.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,o.default)(t,[{key:"render",value:function(){return _.default.createElement(E.default,{title:"Check email",description:"Check email description...",session:this.props.session,__source:{fileName:g,lineNumber:16}},_.default.createElement("h2",{__source:{fileName:g,lineNumber:21}},"Check your email"),_.default.createElement("p",{__source:{fileName:g,lineNumber:22}},"You have been sent an email with a link you can use to sign in."))}}]),t}(v.default);w.propTypes={session:b.default.object.isRequired},t.default=(0,y.default)(N.store)(w)},890:function(e,t,u){e.exports=u(392)}},[890]);
+            return { page: comp.default }
+          })
+        
